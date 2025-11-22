55d23ad48ff5fc2d20cc4f070dca8537a1421f74
the-var <thevar@users.noreply.github.com>
2025-11-22 12:40:28 +0300

    fix: force kestrel to bind to port 8080 for docker

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Force Kestrel to listen on port 8080 inside Docker
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
 
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -14,7 +18,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-//app.UseHttpsRedirection();
+// Disable HTTPS redirection inside Docker
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
